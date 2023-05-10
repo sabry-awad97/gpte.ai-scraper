3442a8c21293abfda6b4006f5f8e6039531fe409
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-10 06:59:00 +0300

    🚀 feat(main.py): Add error handling for HTTP requests To improve the error handling in `WebScraper` class, an error handling logic was added to the `get_page` method to handle `requests.exceptions.RequestException` and HTTP status code errors. Changes made: - A `try/except` block was added to handle the `requests.exceptions.RequestException` error. - An additional check for HTTP status code was added, and if the status code is not 200, an error message will be printed. - The method now returns the `soup` object if the request is successful and `None` if there is an error. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ class WebScraper:
         self.page: bytes | None = None
         self.soup: BeautifulSoup | None = None
         self.data: List[Dict[str, str]] = []
+
+    def get_page(self):
+        try:
+            url = f"{self.url}/page/{self.page_num}/"
+            response = requests.get(url)
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return None
+
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}")
+            return None
+
+        self.page = response.content
+        self.soup = BeautifulSoup(self.page, "html.parser")
+        return self.soup
